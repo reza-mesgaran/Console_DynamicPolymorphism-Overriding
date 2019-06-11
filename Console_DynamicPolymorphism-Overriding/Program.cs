a008f2a60125ecadc3875c23d91c816f1c13b5fe
Reza Mesgaran <reza_mesgaran@live.com>
2019-06-11 19:29:33 -0400

    Add project files.

---
+++ Console_DynamicPolymorphism-Overriding/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_DynamicPolymorphism_Overriding  // Late binding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBaseClass c1 = new MyDerivedClass();
+            c1.MyMethod();                     // Without using polymorphism (Method Overriding) - MyMethod* from MyBaseClass will revoke!
+
+            Console.ReadKey();
+
+        }
+    }
+   class MyBaseClass
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("I am BASE method");
+        }
+    }
+    class MyDerivedClass: MyBaseClass
+    {
+       public void MyMethod()
+        {
+            Console.WriteLine("I am CHILD method");
+        }
+    }
+}
